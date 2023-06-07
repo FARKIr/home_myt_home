6e5d18c6d31975e4c3c614c779c182b590f130ad
FARKIr <farkasroman@hotmail.com>
2023-06-07 23:06:30 +0200

    Update scraper.py
    2.

--- scraper.py
+++ scraper.py
@@ -6,17 +6,37 @@ html = scraperwiki.scrape("https://www.shmu.sk/sk/?page=765&station_id=7472")
 
 # Nájdenie tabuľky s informáciami o stanici
 root = lxml.html.fromstring(html)
-table = root.cssselect("table.center.w500")[0]  # Vyber prvú tabuľku s týmto triedovým selektorom
+table_info = root.cssselect("table.center.w500")[0]  # Tabuľka s informáciami o stanici
 
-# Parsovanie informácií z tabuľky
-data = {}
-for row in table.cssselect("tbody tr"):
+# Parsovanie informácií o stanici
+data_info = {}
+for row in table_info.cssselect("tbody tr"):
     cells = row.cssselect("td")
     if len(cells) == 2:
         attribute = cells[0].text_content().strip().rstrip(":")
         value = cells[1].text_content().strip()
-        data[attribute] = value
+        data_info[attribute] = value
 
-# Výpis získaných informácií
-for attribute, value in data.items():
+# Výpis informácií o stanici
+for attribute, value in data_info.items():
     print(f"{attribute}: {value}")
+print()
+
+# Nájdenie tabuľky s meranými hodnotami vodného stavu
+table_values = root.cssselect("table.merane_hodnoty")[0]  # Tabuľka s meranými hodnotami
+
+# Parsovanie meraných hodnôt vodného stavu
+data_values = []
+header_row = table_values.cssselect("thead tr")[0]
+headers = [header.text_content().strip() for header in header_row.cssselect("th")]
+
+for row in table_values.cssselect("tbody tr"):
+    cells = row.cssselect("td")
+    values = [cell.text_content().strip() for cell in cells]
+    data_values.append(dict(zip(headers, values)))
+
+# Výpis meraných hodnôt vodného stavu
+for row in data_values:
+    for attribute, value in row.items():
+        print(f"{attribute}: {value}")
+    print()
